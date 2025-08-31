24fb788101b258209df8403a0579f126c2d359f7
a.safronov <a.safronov@propellerads.net>
2025-08-31 19:08:31 +0300

    step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,7 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(
         name = "gendiff",
@@ -10,7 +11,27 @@ import picocli.CommandLine.Command;
         mixinStandardHelpOptions = true,
         version = "gendiff 0.1.0"
 )
-public class App {
+public class App implements Runnable {
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish"
+    )
+    private String format;
+
+    @Override
+    public void run() {
+        System.out.printf("Comparing '%s' and '%s' with format '%s'%n",
+                filepath1, filepath2, format);
+    }
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
