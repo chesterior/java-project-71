873302d3ad280d78db5d8e8371538fd420f5abba
a.safronov <a.safronov@propellerads.net>
2025-10-05 03:15:36 +0300

    step final

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public final class DiffBuilder {
     private DiffBuilder() {
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,7 +4,8 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 public final class Differ {
 
